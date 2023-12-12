33efc7d06e1797beddaf6fcbb036b0394058c00d
João Filippe Rossi Rodrigues <joaorodrigues@eulabs.com.br>
2023-12-12 09:58:43 -0300

    feat: adding a prototype of a test

--- common/helpers/vaildate_test.go
+++ common/helpers/vaildate_test.go
@@ -1,12 +1,48 @@
 package helpers_test
 
 import (
+	"fmt"
 	"testing"
 	"todoGo/common/helpers"
-
-	"gorm.io/gorm/utils/tests"
 )
 
 func Test_CheckEmptyFields(t *testing.T) {
+	tests := []struct {
+		name string
+		data any
+		want bool
+	}{
+		{
+			name: "empty string",
+			data: "",
+			want: true,
+		},
+		{
+			name: "empty struct",
+			data: struct{}{},
+			want: true,
+		},
+		{
+			name: "empty struct with pointer",
+			data: struct {
+				Pointer *string
+			}{},
+		},
+		{
+			name: "struct with one empty field",
+			data: struct {
+				name string
+				Pointer *string
+			}{},
+		},
+	}
 
+	for _, tt := range tests {
+		res, err := helpers.CheckEmptyFields(tt.data)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		fmt.Println(res)
+	}
 }
