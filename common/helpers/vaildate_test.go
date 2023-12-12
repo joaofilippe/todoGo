129d3c0ac64269c74b1b83da1eba4eb02e25c062
João Filippe Rossi Rodrigues <joaorodrigues@eulabs.com.br>
2023-12-12 12:13:02 -0300

    feat: finishin tests strucuture

--- common/helpers/vaildate_test.go
+++ common/helpers/vaildate_test.go
@@ -1,48 +1,59 @@
 package helpers_test
 
 import (
-	"fmt"
 	"testing"
 	"todoGo/common/helpers"
 )
 
 func Test_CheckEmptyFields(t *testing.T) {
 	tests := []struct {
-		name string
-		data any
-		want bool
+		name   string
+		data   any
+		hasErr    bool
+		emptyFields int
 	}{
 		{
-			name: "empty string",
-			data: "",
-			want: true,
+			name:   "empty string",
+			data:   "",
+			hasErr:    true,
+			emptyFields: 0,
 		},
 		{
-			name: "empty struct",
-			data: struct{}{},
-			want: true,
+			name:   "empty struct",
+			data:   struct{}{},
+			hasErr:    true,
+			emptyFields: 0,
 		},
 		{
 			name: "empty struct with pointer",
 			data: struct {
-				Pointer *string
-			}{},
+				Pointer *[]string
+			}{
+			},
+			hasErr:    false,
+			emptyFields: 1,
 		},
 		{
 			name: "struct with one empty field",
 			data: struct {
-				name string
-				Pointer *string
-			}{},
+				name    string
+				age     int
+			}{
+				name:   "João Filippe",
+			},
+			hasErr:    false,
+			emptyFields: 1,
 		},
 	}
 
 	for _, tt := range tests {
 		res, err := helpers.CheckEmptyFields(tt.data)
-		if err != nil {
-			fmt.Println(err)
+		if err != nil && !tt.hasErr {
+			t.Error(err)
 		}
 
-		fmt.Println(res)
+		if len(res) != tt.emptyFields {
+			t.Errorf("expected %d empty fields, got %d", tt.emptyFields, len(res))
+		}
 	}
 }
