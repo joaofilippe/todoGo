a128384e12271c57b52563efc431ac4cf8b6af96
João Filippe Rossi Rodrigues <joaofilippe@outlook.com>
2024-01-04 13:05:55 -0300

    feat: connecting to db

--- adapters/repository/postgres/config.go
+++ adapters/repository/postgres/config.go
@@ -8,11 +8,11 @@ import (
 )
 
 type Config struct {
-	Host     string
-	Port     int
-	User     string
-	Password string
-	DBName   string
+	Host     string `yaml:"host"`
+	Port     int    `yaml:"port"`
+	User     string `yaml:"user"`
+	Password string `yaml:"password"`
+	DBName   string `yaml:"dbname"`
 	Dsn      string
 }
 
--- main.go
+++ main.go
@@ -5,25 +5,61 @@ import (
 	"log"
 	"os"
 
-	_ "github.com/lib/pq"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"gopkg.in/yaml.v3"
 
+	"github.com/joaofilippe/todoGo/adapters/repository/postgres"
 	common "github.com/joaofilippe/todoGo/common/enum"
 	"github.com/joaofilippe/todoGo/common/logger"
 )
+
 func main() {
+	env := loadEnv()
+
+	logger := logger.NewLogger(logger.LogOptions{
+		Environment: env.ToString(),
+	})
+
+	logger.InfoF("logando")
+
+	masterConnectionDB := getDbConnection("master")
+	slaveConnectionDB := getDbConnection("slave")
+
+	fmt.Println(masterConnectionDB, slaveConnectionDB)
+}
+
+func loadEnv() common.Environment{
 	err := godotenv.Load("./config/.env")
 	if err != nil {
 		log.Fatal("can't load .env file")
 	}
 
-	env := common.ParseToEnviroment(os.Getenv("ENV"))
-	fmt.Println(env)
-
-	logger := logger.NewLogger(logger.LogOptions{})
-
-	logger.InfoF("logando")
-
+	return common.ParseToEnviroment(os.Getenv("ENV"))
 }
 
+func getDbConnection(c string) *postgres.Connection {
+	yamlFile, err := os.ReadFile(fmt.Sprintf("./config/%s.yaml", c))
+	if err != nil {
+		log.Fatalf("can't load %s.yaml file", c)
+	}
+
+	configDB := new(postgres.Config)
+
+	err = yaml.Unmarshal(yamlFile, configDB)
+	if err != nil {
+		log.Fatalf("can't unmarshal %s.yaml file", c)
+	}
+
+	conn := postgres.NewConnection(configDB)
+	defer conn.Connection.Close()
+	
+	if err := conn.Connection.Ping(); err != nil {
+		log.Fatal(fmt.Sprintf("can't connect to %s database", c))
+	} else {
+		log.Println(fmt.Sprintf("connected to %s database", c))
+	}
+
+	return conn
+}
 
