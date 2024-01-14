62925c299994a308e83a9ce4acb9a29e90cc6796
João Filippe Rossi Rodrigues <joaofilippe@outlook.com>
2024-01-14 15:30:12 -0300

    feat: moduling migration

--- migration/user/user.go
+++ migration/user/user.go
@@ -2,32 +2,6 @@ package migration
 
 import "github.com/joaofilippe/todoGo/adapters/database/postgres"
 
-
-const (
-	// CreateUserTableQuery is the query to create the user table
-	CreateUserTableQuery = `
-			CREATE TABLE IF NOT EXISTS public.user
-		(
-		    id         UUID                                NOT NULL
-		        CONSTRAINT id_user_pk
-		            PRIMARY KEY,
-		    first_name VARCHAR                             NOT NULL,
-		    last_name  VARCHAR                             NOT NULL,
-		    username   VARCHAR                             NOT NULL,
-		    email      VARCHAR                             NOT NULL,
-		    password   VARCHAR                             NOT NULL,
-		    todo_ids   UUID[],
-		    birthdate  DATE                                NOT NULL,
-		    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-		    updated_at TIMESTAMP,
-		    active     BOOL      DEFAULT FALSE             NOT NULL
-		);
-	`	
-
-	// DropUserTableQuery is the query to drop the user table
-	DropUserTableQuery = "DROP TABLE IF EXISTS user"
-)
-
 // CreateUsersTable creates the users table
 func CreateUsersTable(conn *postgres.Connection) error {
 	_, err := conn.Connection.Exec(CreateUserTableQuery)
